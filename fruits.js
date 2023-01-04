138ee3551595f8a4737ccdaf339ba72767b1bad2
Coding-is-fun123 <99051307+Coding-is-fun123@users.noreply.github.com>
2023-01-04 10:56:49 -0500

    Add files via upload

---
+++ fruits.js
@@ -0,0 +1,25 @@
+status="";
+function preload(){
+    img= loadImage("fruits.jpg");
+}
+function setup(){
+    canvas= createCanvas(700,600);
+    canvas.center();
+    objectDetector= ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML= "STATUS : DETECTING OBJECTS";
+    
+}
+function modelLoaded(){
+    console.log("MODEL IS LOADED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    status= true;
+    objectDetector.detect(img, gotResult);
+
+}
+
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+
+    }
+    console.log(results);
+}
